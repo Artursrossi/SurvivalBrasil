579e1ab152911bc39892a69474c067fab625dfc9
artursrossi <artursrossi4@gmail.com>
2021-02-12 08:49:10 -0300

    survivalbrasil

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-import 'artur.css'
+import Head from 'next/Head'
 
 function Home() {
-    return <div class="AmainbarA"></div>
+    return(
+        <div class="Aheader">
+            <Head>
+                <title>Teste</title>
+            </Head>
+            <style>
+                
+            </style>
+            <div class="Amainbar">
+                <h3>
+                Teste
+                </h3>
+            </div>
+        </div>
+    )
 }
 
 export default Home
