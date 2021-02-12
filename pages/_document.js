57fbecfe36d92f50988d1724a5fa33cc3202d84c
Artursrossi <78820818+Artursrossi@users.noreply.github.com>
2021-02-12 08:07:06 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,16 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html>
+        <Head>
+          <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "5dy08bis9z");
+          </script>
+        </Head>
         <body>
           <Main />
           <NextScript />
