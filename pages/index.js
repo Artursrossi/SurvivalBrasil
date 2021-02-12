7c520a3e2998ee0d67b2b3740b27d0c517f277cd
Artursrossi <78820818+Artursrossi@users.noreply.github.com>
2021-02-12 07:58:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import Head from 'next/head'
+
 function Home() {
     return <div>teste</div>
 }
 
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
 export default Home
+export default IndexPage
